75001eefe423f29268b1f8f680fa885a947fc46c
Christian Fuchs <58251727+MrFox09@users.noreply.github.com>
2020-02-03 16:56:20 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ const colorOptions = document.querySelector('#color');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#mail');
 const creditCardNumberInput = document.querySelector('#cc-num');
-const zipInput =  document.querySelector('#zip');
-const cvvInput =  document.querySelector('#cvv');
+const zipInput = document.querySelector('#zip');
+const cvvInput = document.querySelector('#cvv');
 
 //*** Job-Role Section ***//
 
 // set the focus to the first input field "Name" with jQuery
-$( document ).ready( () => {
-  $( "#name" ).focus();
+$(document).ready(() => {
+  $("#name").focus();
 });
 
 
@@ -41,43 +41,43 @@ jobRoleOptions.addEventListener('change', () => {
 //to show the right T-shirt colors depending on which design is chosen
 const colorDiv = document.querySelector('#colors-js-puns');
 
-const selectColorOptions = (design) =>   {
-  for (let i=0; i< color.length; i++){
+const selectColorOptions = (design) => {
+  for (let i = 0; i < color.length; i++) {
 
 
 
-    if (design === 'Select Theme'){
-      colorDiv.style.display ='none';
+    if (design === 'Select Theme') {
+      colorDiv.style.display = 'none';
 
       color[i].hidden = true;
       color[0].selected = true;
-      color[0].textContent ='Please select a design';
+      color[0].textContent = 'Please select a design';
 
 
-   }else if(design === 'js puns'){
-       colorDiv.style.display ='';
-       color[i].hidden = true;
-       color[0].selected = true;
-       color[0].textContent ='Cornflower Blue (JS Puns shirt only)';
-       color[0].hidden = false;
-       color[1].hidden = false;
-       color[2].hidden = false;
+    } else if (design === 'js puns') {
+      colorDiv.style.display = '';
+      color[i].hidden = true;
+      color[0].selected = true;
+      color[0].textContent = 'Cornflower Blue (JS Puns shirt only)';
+      color[0].hidden = false;
+      color[1].hidden = false;
+      color[2].hidden = false;
 
 
 
-     }else if(design === 'heart js'){
-       colorDiv.style.display ='';
-       color[i].hidden = true;
-       color[0].selected = true;
-       color[0].innerHTML = 'Tomato (I &#9829; JS shirt only)';
-       color[3].hidden = false;
-       color[4].hidden = false;
-       color[5].hidden = false;
+    } else if (design === 'heart js') {
+      colorDiv.style.display = '';
+      color[i].hidden = true;
+      color[0].selected = true;
+      color[0].innerHTML = 'Tomato (I &#9829; JS shirt only)';
+      color[3].hidden = false;
+      color[4].hidden = false;
+      color[5].hidden = false;
 
 
-      }else {
-        selectColorOptions('Select Theme');
-      }
+    } else {
+      selectColorOptions('Select Theme');
+    }
 
 
   }
@@ -88,9 +88,9 @@ selectColorOptions('Select Theme');
 // event Listener for the designOptions and call the selectColorOptions function
 // to show the right T-shirt colors
 
-designOptions.addEventListener('change', (event)=>{
+designOptions.addEventListener('change', (event) => {
 
-    selectColorOptions(event.target.value);
+  selectColorOptions(event.target.value);
 
 
 
@@ -127,7 +127,7 @@ activityFieldset.addEventListener('change', (event) => {
 
     totalCost = totalCost + parseInt(clickedTypeCost, 10);
     lastCostDiv.textContent = 'Total $ ' + totalCost;
-    document.querySelector('#error-checkbox').style.display ='none';
+    document.querySelector('#error-checkbox').style.display = 'none';
 
   } else if (clicked.checked !== true) {
 
@@ -147,19 +147,10 @@ activityFieldset.addEventListener('change', (event) => {
       //and the option is clicked disable the "other" button(s)
       if (clicked.checked) {
         registerActivities[i].disabled = true;
-        registerActivities[i].classList = 'colision';
-        console.log(registerActivities[i]);
-
-
 
       } else {
 
         registerActivities[i].disabled = false;
-        registerActivities[i].classList.remove('colision');
-
-        console.log(registerActivities[i]);
-
-
 
       }
     }
@@ -182,8 +173,8 @@ const selectMethodOption = document.querySelector('option[value="select method"]
 const creditCardOption = document.querySelector('option[value="credit card"]');
 selectMethodOption.style.display = 'none';
 creditCardOption.selected = true;
-paypalDiv.style.display ='none';
-bitcoinDiv.style.display ='none';
+paypalDiv.style.display = 'none';
+bitcoinDiv.style.display = 'none';
 
 // Event Listener for payment section
 
@@ -192,21 +183,21 @@ paymentOptions.addEventListener('change', (event) => {
   const clicked = event.target.value;
 
   if (clicked === 'credit card') {
-    creditCardDiv.style.display ='';
-    bitcoinDiv.style.display ='none';
-    paypalDiv.style.display ='none';
+    creditCardDiv.style.display = '';
+    bitcoinDiv.style.display = 'none';
+    paypalDiv.style.display = 'none';
 
 
   } else if (clicked === 'paypal') {
-    creditCardDiv.style.display ='none';
-    bitcoinDiv.style.display ='none';
-    paypalDiv.style.display ='';
+    creditCardDiv.style.display = 'none';
+    bitcoinDiv.style.display = 'none';
+    paypalDiv.style.display = '';
 
 
   } else if (clicked === 'bitcoin') {
-    creditCardDiv.style.display ='none';
-    paypalDiv.style.display ='none';
-    bitcoinDiv.style.display ='';
+    creditCardDiv.style.display = 'none';
+    paypalDiv.style.display = 'none';
+    bitcoinDiv.style.display = '';
 
 
   }
@@ -273,14 +264,14 @@ const isCreditCardValid = (input) => {
 const isZipValid = (input) => {
 
   if (creditCardOption.selected === true) {
-    return  zipRegex.test(input);
+    return zipRegex.test(input);
   }
 };
 
 const isCvvValid = (input) => {
 
   if (creditCardOption.selected === true) {
-    return  cvvRegex.test(input);
+    return cvvRegex.test(input);
   }
 };
 
@@ -288,31 +279,36 @@ const isCvvValid = (input) => {
 // eventListener for the submit button,show the error message when its fail
 
 document.querySelector('button[type="submit"]').addEventListener('click', (event) => {
-// call the validation functions and if anyone is false it don't sumbit the form
-  if (isNameValid(nameInput.value) === false ) {
+  // call the validation functions and if anyone is false it don't sumbit the form
+  if (isNameValid(nameInput.value) === false) {
     event.preventDefault();
-    showOrHideErrorMessage(true,nameInput.previousElementSibling);
+    showOrHideErrorMessage(true, nameInput.previousElementSibling);
 
-  }if (isEmailValid(emailInput.value) === false ) {
+  }
+  if (isEmailValid(emailInput.value) === false) {
     event.preventDefault();
-    showOrHideErrorMessage(true,emailInput.previousElementSibling);
+    showOrHideErrorMessage(true, emailInput.previousElementSibling);
 
-  }if (isCheckboxChecked() === false ) {
+  }
+  if (isCheckboxChecked() === false) {
     event.preventDefault();
-    showOrHideErrorMessage(true,document.querySelector('#error-checkbox'));
+    showOrHideErrorMessage(true, document.querySelector('#error-checkbox'));
 
-  }if(creditCardNumberInput.value === '' || isCreditCardValid(creditCardNumberInput.value) === false  ){
+  }
+  if (creditCardNumberInput.value === '' || isCreditCardValid(creditCardNumberInput.value) === false) {
     event.preventDefault();
     document.querySelector('#error-creditcard').textContent = "Cant't be blank, it should have 13-16 Numbers";
-    showOrHideErrorMessage(true,creditCardNumberInput.previousElementSibling);
+    showOrHideErrorMessage(true, creditCardNumberInput.previousElementSibling);
 
-  }if (isZipValid(zipInput.value) === false ) {
+  }
+  if (isZipValid(zipInput.value) === false) {
     event.preventDefault();
-    showOrHideErrorMessage(true,zipInput.previousElementSibling);
+    showOrHideErrorMessage(true, zipInput.previousElementSibling);
 
-  }if (isCvvValid(cvvInput.value) === false ) {
+  }
+  if (isCvvValid(cvvInput.value) === false) {
     event.preventDefault();
-    showOrHideErrorMessage(true,cvvInput.previousElementSibling);
+    showOrHideErrorMessage(true, cvvInput.previousElementSibling);
   }
 });
 
@@ -320,35 +316,35 @@ document.querySelector('button[type="submit"]').addEventListener('click', (event
 
 // function to create Messages
 
-const errorMessage = (message,parent,addDivTo, IdName)=>{
+const errorMessage = (message, parent, addDivTo, IdName) => {
   const newDiv = document.createElement('div');
   newDiv.setAttribute('ID', IdName);
   newDiv.className = 'error';
   newDiv.textContent = message;
-  parent.insertBefore(newDiv,addDivTo);
-  newDiv.style.display ='none';
+  parent.insertBefore(newDiv, addDivTo);
+  newDiv.style.display = 'none';
 
 };
 
 //error Message for the Name Field
-errorMessage("Can't be blank", document.querySelector('fieldset'),nameInput ,'error-name');
+errorMessage("Can't be blank", document.querySelector('fieldset'), nameInput, 'error-name');
 
 //error Message for the Email Field
-errorMessage('Please enter a valid Email', document.querySelector('fieldset'),emailInput ,'error-email');
+errorMessage('Please enter a valid Email', document.querySelector('fieldset'), emailInput, 'error-email');
 
 //error Message for the registerActivities
-errorMessage('Select at least one activity', activityFieldset, document.querySelector('fieldset[class="activities"] label') ,'error-checkbox');
+errorMessage('Select at least one activity', activityFieldset, document.querySelector('fieldset[class="activities"] label'), 'error-checkbox');
 
 //error Message for the CreditCard Number Field and the Zip Code Field the CVV Field
-errorMessage('13-16 Numbers only', document.querySelector('div[class="col-6 col"]'),creditCardNumberInput, 'error-creditcard');
+errorMessage('13-16 Numbers only', document.querySelector('div[class="col-6 col"]'), creditCardNumberInput, 'error-creditcard');
 
 
-errorMessage('5 Numbers only', document.querySelector('div[class="col-3 col"]'),zipInput, 'error-zip');
+errorMessage('5 Numbers only', document.querySelector('div[class="col-3 col"]'), zipInput, 'error-zip');
 
 
-errorMessage('3 Numbers only', document.querySelector('div[logic="jscvv"]'),cvvInput, 'error-cvv');
+errorMessage('3 Numbers only', document.querySelector('div[logic="jscvv"]'), cvvInput, 'error-cvv');
 
- // show element when show is true, hide when false
+// show element when show is true, hide when false
 const showOrHideErrorMessage = (show, element) => {
   if (show) {
     element.style.display = '';
